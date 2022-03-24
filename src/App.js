f23be76975e123313cddb280ce059dfa68219548
shariarhossain23e <shariarhossain23@gamil.comm>
2022-03-24 16:18:22 +0600

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Gunshop from './component/Gunshop/Gunshop';
+import Guns from './component/Gun/Guns';
 
 function App() {
-  return (
-    <div>
-    <Gunshop></Gunshop>
-    </div>
-  );
+  const [guns,setGuns] = useState([])
+  useEffect(()=>{
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => setGuns(data))
+  },[])
+    return (
+        <div>
+            <div className='gun'>
+                {
+                    guns.map(gun => <Guns key={gun.id} gun={gun}></Guns> )
+                }
+            </div>
+        </div>
+    );
+  
 }
 
 export default App;
